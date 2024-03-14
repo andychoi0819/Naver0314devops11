b029b1eff7b6b663c48ec721ff8d68d6579a2589
andychoi <schoi43@gmu.edu>
2024-03-14 16:38:15 +0900

    메세지

---
+++ Javawork/Naver0314JavaProject/src/day0314/Test.java
@@ -0,0 +1,10 @@
+package day0314;
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		String Date="0314";
+		System.out.println("오늘은 네이버 데브옵스 11기 개강날입니다");
+		System.out.println(Date);
+	}
+}
