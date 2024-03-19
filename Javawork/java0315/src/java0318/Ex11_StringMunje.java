74fea2f42f58577e3fc3af1890498fed18d13fd9
andychoi <schoi43@gmu.edu>
2024-03-19 17:32:29 +0900

    0319 Fin

---
+++ Javawork/java0315/src/java0318/Ex11_StringMunje.java
@@ -0,0 +1,38 @@
+package java0318;
+import java.util.Scanner;
+public class Ex11_StringMunje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String msg;
+		int up=0,low=0,num=0;
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("입력하시오");
+		msg=sc.nextLine();
+		
+		
+		for(int i=0; i<msg.length(); i++) 
+		{
+			
+			if(msg.charAt(i)>='A'&& msg.charAt(i)<='Z')
+			{
+			 up++;
+			}
+			if(msg.charAt(i)>='a'&& msg.charAt(i)<='z')
+			{
+			 low++;
+			}
+			if((msg.charAt(i)>='0'&& msg.charAt(i)<='9')) 
+			{
+			 num++;
+			}
+			
+			
+		}
+		
+		System.out.printf("대문자:%d, 소문자: %d, 숫자: %d",up, low,num);
+			
+	}
+
+}
